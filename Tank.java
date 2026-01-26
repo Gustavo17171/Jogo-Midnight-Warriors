8f706665abce6433efd57be460e86d823fbdbf51
João Carlos <190541239+JOc442@users.noreply.github.com>
2026-01-26 13:59:20 -0300

    Update Tank.java
    .

--- Tank.java
+++ Tank.java
@@ -4,47 +4,40 @@ import java.awt.*;
 
 
 public abstract class Tank {
-    private String Guardião_de_ferro;
+    private String Guardiao_de_ferro;
     private int vigor;
     private int vida;
     private int atqBasico;
     private int Bloqueio;
     private int identidade;
 
-public Tank (String Guardião_de_ferro, int vigor, int vida, int atqBasico, int Bloqueio,int identidade) {
-    this.Guardião_de_ferro = Guardião_de_ferro;
+public Tank (String Guardiao_de_ferro, int vigor, int vida, int atqBasico, int Bloqueio,int identidade) {
+    this.Guardião_de_ferro = Guardiao_de_ferro;
     this.vigor = vigor;
     this.vida = vida;
     this.atqBasico = atqBasico;
     this.Bloqueio = Bloqueio;
     this.identidade = identidade;
 }
-
-public int Ataque(){  
-        vigor += 25;
-        return atqBasico;
-        }
     
-public int Habilidade_Unica() {
-    if (vigor >= 30) { 
-        vigor -= 30;  
-        return Bloqueio;
-    }
-    else { vigor += 25;            
-        return atqBasico;
-    }
-}
+public int ataque() {
 
+    if (vigor <= 100) { 
+    vigor -= 100;  
+    return Bloqueio;
+    }
+    else { vigor += 50;            
+    return atqBasico;
 }
 
 class Tank_Luz extends Tank {
     public Tank_Luz(int identidade){
-    super("Guardiao de ferro da Luz", 100, 200, 20, 300,identidade);
+    super("Guardiao de ferro da Luz", 100, 120, 20, 300,identidade);
     }    
 }
 
 class Tank_Sombra extends Tank {
     public Tank_Sombra(int identidade){    
-    super("Guardiao de ferro da Sombra",100, 200, 20, 300,identidade);
+    super("Guardiao de ferro da Sombra",100, 120, 20, 300,identidade);
     }
 }
